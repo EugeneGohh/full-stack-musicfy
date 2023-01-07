0268f8b6b83b32876d4ec525007e7aae948535d1
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2023-01-07 11:33:22 +0800

    feat: add page layout ui

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import PlayerLayout from "../components/playerLayout";
+
+const Home = () => {
+  return (
+    <div>
+      <PlayerLayout children="Home" />
+      home
+    </div>
+  );
+};
+
+export default Home;
