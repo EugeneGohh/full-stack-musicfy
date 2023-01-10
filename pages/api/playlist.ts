06421844d8a7c3c64da66c066d22502817131b30
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2023-01-10 09:23:24 +0800

    feat: restructure api & middleware code

--- pages/api/playlist.ts
+++ pages/api/playlist.ts
@@ -3,9 +3,9 @@ import { validateRoute } from "../../lib/auth";
 
 export default validateRoute(async (req, res, user) => {
   const playlists = await prisma.playlist.findMany({
-    // where: {
-    //   userId: user.id,
-    // },
+    where: {
+      userId: user.id,
+    },
     orderBy: {
       name: "asc",
     },
