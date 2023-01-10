c23f62bb6336ad1c06569507196dc4998b7d0d22
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2023-01-10 09:23:06 +0800

    feat: build ui component for home page

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,22 +1,19 @@
-import React from "react";
 import { Box } from "@chakra-ui/layout";
-import SideBar from "./sidebar";
+import Sidebar from "./sidebar";
+// import PlayerBar from "./playerBar";
 
 const PlayerLayout = ({ children }) => {
   return (
-    /* 100% on width & height depending on screen */
     <Box width="100vw" height="100vh">
-      <Box position="absolute" top="0" width="250px">
-        <SideBar />
+      <Box position="absolute" top="0" width="250px" left="0">
+        <Sidebar />
       </Box>
-
       <Box marginLeft="250px" marginBottom="100px">
-        {children}
+        <Box height="calc(100vh - 100px)">{children}</Box>
       </Box>
-
-      <Box position="absolute" left="0" bottom="0">
-        player
-      </Box>
+      {/* <Box position="absolute" left="0" bottom="0">
+        <PlayerBar />
+      </Box> */}
     </Box>
   );
 };
