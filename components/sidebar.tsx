a0e3a4d0cfdef87653cbfb1b6d8904398cd90fac
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2023-01-12 12:12:19 +0800

    feat: add ui layout & fetch artist data from db

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -49,10 +49,10 @@ const navMenu = [
   },
 ];
 
-// const playlists = new Array(30).fill(1).map((_, i) => `Plyalist ${i + 1}`);
+const playlists = new Array(30).fill(1).map((_, i) => `Plyalist ${i + 1}`);
 
 const SideBar = () => {
-  const { playlists } = usePlaylist();
+  // const { playlists } = usePlaylist();
 
   return (
     <Box
@@ -116,10 +116,10 @@ const SideBar = () => {
         <Box height="66%" overflowY="auto" paddingY="20px">
           <List spacing={2}>
             {playlists.map((playlist) => (
-              <ListItem paddingX="20px" key={playlist.id}>
+              <ListItem paddingX="20px" key={playlist}>
                 <LinkBox>
                   <Link href="/" passHref>
-                    <LinkOverlay>{playlist.name}</LinkOverlay>
+                    <LinkOverlay>{playlist}</LinkOverlay>
                   </Link>
                 </LinkBox>
               </ListItem>
